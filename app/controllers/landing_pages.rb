c16950b4eda6173b337610fc07a8e8775bc6183b
adrian dabi <adrian.dabi@hotmail.com>
2018-09-18 09:52:45 -0700

    fixing home

--- app/controllers/landing_pages.rb
+++ app/controllers/landing_pages.rb
@@ -1,5 +1,20 @@
 class SimplePagesController < ApplicationController
   def landing_page
     @featured_product = Product.first
-  end
+    @products = Product.limit(3)
+end
+
+def index
+end
+
+def contact
+end
+
+def thank_you
+@name = params[:name]
+@email = params[:email]
+@message = params[:message]
+UserMailer.contact_form(@email, @name, @message).deliver_now
+end
+
 end
