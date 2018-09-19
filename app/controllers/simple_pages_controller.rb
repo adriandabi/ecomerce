48c4d7a5922739e88a5ea63afd3ff9bb96687018
adrian dabi <adrian.dabi@hotmail.com>
2018-09-19 13:45:14 -0700

    push to master

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -4,13 +4,4 @@ class SimplePagesController < ApplicationController
 
   end
 
-  def thank_you
-    @name = params[:name]
-    @email = params[:email]
-    @message = params[:message]
-    ActionMailer::Base.mail(from: @email,
-        to: 'julia@julia-hofmann.com',
-        subject: "A new contact form message from #{@name}",
-        body: @message).deliver_now
-    end
 end
