3eeb6f03f967df7f3f3449ff67c28b34ad70be7e
adrian dabi <adrian.dabi@hotmail.com>
2018-09-23 18:02:34 -0700

    adding mailers AND Authentication and users

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -3,5 +3,10 @@ class SimplePagesController < ApplicationController
     @featured_product = Product.limit(3)
 
   end
-
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  UserMailer.contact_form(@email, @name, @message).deliver_now
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users
   resources :products
   resources :object_names
   resources :orders, only: [:index, :show, :create, :destroy]
@@ -6,6 +8,6 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
   get 'simple_pages/index'
   root 'simple_pages#landing_page'
-
+  post 'simple_pages/thank_you'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
